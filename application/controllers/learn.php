bb1c3db4d0022b057cdfa4e826937768fdb99943
AWP <AWP>
2017-12-14 20:01:02 +0700

    first commit

---
+++ application/controllers/learn.php
@@ -0,0 +1,9 @@
+<?php 
+
+class Learn extends CI_Controller{
+
+	function index(){
+		$this->load->view('learn');
+	}
+	
+}
---
+++ application/views/home.php
@@ -0,0 +1,263 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Bootsrap dan Jquery Framework</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FREEHTML5.CO" />
+
+  <!-- 
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by FREEHTML5.CO
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<!-- Google Webfonts -->
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100,500' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,300,100,500' rel='stylesheet' type='text/css'>
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="../cibootjque/assets/css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="../cibootjque/assets/css/icomoon.css">
+	<!-- Simple Line Icons -->
+	<link rel="stylesheet" href="../cibootjque/assets/css/simple-line-icons.css">
+	<!-- Theme Style -->
+	<link rel="stylesheet" href="../cibootjque/assets/css/style.css">
+	<!-- Modernizr JS -->
+	<script src="../cibootjque/assets/js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<header id="fh5co-header" role="banner">
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 col-md-offset-1">
+						<div class="navbar-header"> 
+						<!-- Mobile Toggle Menu Button -->
+						<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle visible-xs-block" data-toggle="collapse" data-target="#fh5co-navbar" aria-expanded="false" aria-controls="navbar"><i></i></a>
+						<a class="navbar-brand" href="index.html">AWP</a>
+						</div>
+						<div id="fh5co-navbar" class="navbar-collapse collapse">
+							<ul class="nav navbar-nav navbar-right">
+								<li class="active"><a href="<?php echo base_url('home'); ?>"><span>Home <span class="border"></span></span></a></li>
+								<li class="active"><a href="<?php echo base_url('learn'); ?>"><span>Learn <span class="border"></span></span></a></li>
+								<li class="active"><a href="<?php echo base_url(''); ?>"><span>About Us <span class="border"></span></span></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<!-- END .header -->
+	
+	
+	<div id="fh5co-main">
+		<div class="fh5co-intro text-center">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<h1 class="intro-lead">Belajar Bootstrap dan Jquery</h1>
+						<p class="">100% Paham by <a href="http://freehtml5.co/" target="_blank">AWP</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="fh5co-portfolio">
+			<div class="fh5co-portfolio-item ">
+				<div class="fh5co-portfolio-figure animate-box" style="background-image: url(../cibootjque/assets/images/work_1.jpg);"></div>
+				<div class="fh5co-portfolio-description">
+					<h2>Bootstrap</h2>
+					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+					<p><a href="#" class="btn btn-primary">Mari Belajar</a></p>
+				</div>
+			</div>
+			<div class="fh5co-portfolio-item fh5co-img-right">
+				<div class="fh5co-portfolio-figure animate-box" style="background-image: url(../cibootjque/assets/images/work_2.jpg);"></div>
+				<div class="fh5co-portfolio-description">
+					<h2>Jquery</h2>
+					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+					<p><a href="#" class="btn btn-primary">Mari Belajar</a></p>
+				</div>
+			</div>
+		</div>
+
+<!--		<div id="fh5co-team">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 col-md-offset-1">
+						<div class="row">
+							<h2 class="section-lead text-center">Leadership</h2>
+							<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-staff to-animate">
+								<figure>
+								<img src="../cibootjque/assets/images/user.jpg" alt="Free HTML5 Template by FREEHTML5.co" class="img-responsive">
+								</figure>
+								<h3>Will Barrow</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+								<ul class="fh5co-social">
+									<li><a href="#"><i class="icon-twitter"></i></a></li>
+									<li><a href="#"><i class="icon-github"></i></a></li>
+								</ul> 	
+							</div>
+							<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-staff to-animate">
+								<figure>
+								<img src="../cibootjque/assets/images/user_2.jpg" alt="Free HTML5 Template by FREEHTML5.co" class="img-responsive">
+								</figure>
+								<h3>Max Conversion</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+								<ul class="fh5co-social">
+									<li><a href="#"><i class="icon-twitter"></i></a></li>
+									<li><a href="#"><i class="icon-github"></i></a></li>
+								</ul> 	
+							</div>
+							<div class="clearfix visible-sm-block visible-xs-block"></div>
+							<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-staff to-animate">
+								<figure>
+								<img src="../cibootjque/assets/images/user_3.jpg" alt="Free HTML5 Template by FREEHTML5.co" class="img-responsive">
+								</figure>
+								<h3>Hanson Deck</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+								<ul class="fh5co-social">
+									<li><a href="#"><i class="icon-twitter"></i></a></li>
+									<li><a href="#"><i class="icon-github"></i></a></li>
+								</ul> 	
+							</div>
+							<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-staff to-animate">
+								<figure>
+								<img src="../cibootjque/assets/images/user.jpg" alt="Free HTML5 Template by FREEHTML5.co" class="img-responsive">
+								</figure>
+								<h3>Sue Shei</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+								<ul class="fh5co-social">
+									<li><a href="#"><i class="icon-twitter"></i></a></li>
+									<li><a href="#"><i class="icon-github"></i></a></li>
+								</ul> 	
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+-->
+		<div id="fh5co-services">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 col-md-offset-1">
+						<div class="row">
+							<h2 class="section-lead text-center">Kelebihan Bootstrap dan Jquery</h2>
+							<div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 fh5co-service">
+								<div class="fh5co-icon to-animate"><i class="icon-present"></i></div>
+								<div class="fh5co-desc">
+									<h3>100% Free</h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+								</div>	
+							</div>
+							<div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 fh5co-service">
+								<div class="fh5co-icon to-animate"><i class="icon-eye"></i></div>
+								<div class="fh5co-desc">
+									<h3>Mudah Digunakan</h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+								</div>
+							</div>
+							<div class="clearfix visible-sm-block visible-xs-block"></div>
+							<div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 fh5co-service">
+								<div class="fh5co-icon to-animate"><i class="icon-crop"></i></div>
+								<div class="fh5co-desc">
+									<h3>Efisien</h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+								</div>
+							</div>
+
+							<div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 fh5co-service">
+								<div class="fh5co-icon to-animate"><i class="icon-speedometer"></i></div>
+								<div class="fh5co-desc">
+									<h3>Menghemat Waktu</h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+								</div>	
+							</div>
+							<div class="clearfix visible-sm-block visible-xs-block"></div>
+							<div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 fh5co-service">
+								<div class="fh5co-icon to-animate"><i class="icon-heart"></i></div>
+								<div class="fh5co-desc">
+									<h3>Mudah dicari</h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+								</div>
+							</div>
+							<div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 fh5co-service">
+								<div class="fh5co-icon to-animate"><i class="icon-umbrella"></i></div>
+								<div class="fh5co-desc">
+									<h3>Kekinian</h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+								</div>
+							</div>
+							<div class="clearfix visible-sm-block visible-xs-block"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<footer id="fh5co-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1 text-center">
+					<p>&copy; CIBOOTJQUE <br>Created by <a href="" target="_blank">AWP</a> Images: <a href="" target="_blank">Pexels</a></p>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+
+	<!-- jQuery -->
+	<script src="../cibootjque/assets/js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="../cibootjque/assets/js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="../cibootjque/assets/js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="../cibootjque/assets/js/jquery.waypoints.min.js"></script>
+	<!-- Main JS -->
+	<script src="../cibootjque/assets/js/main.js"></script>
+
+	
+	</body>
+</html>
